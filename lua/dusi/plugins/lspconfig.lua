184b926bd650eeb09532d3e6232b2c2abe45c70c
Dusernajder <dusungulu.tibi@gmail.com>
2025-02-22 16:03:07 +0100

    global/local buffer lsp keymaps

--- lua/dusi/plugins/lspconfig.lua
+++ lua/dusi/plugins/lspconfig.lua
@@ -18,6 +18,7 @@ return {
         config = function()
             local lspconfig = require("lspconfig")
             local util = require("lspconfig.util")
+
             lspconfig.lua_ls.setup({})
             lspconfig.csharp_ls.setup({
                 cmd = { vim.fn.stdpath("data") .. "/mason/bin/csharp-ls" },
@@ -26,6 +27,39 @@ return {
                     return util.root_pattern '*.sln'(fname) or util.root_pattern '*.csproj'(fname)
                 end,
                 filetypes = { "cs" },
+            })
+
+            -- Buffer global keymaps
+            vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
+            vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+            vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+            vim.keymap.set('n', '<leader>q', vim.diagnostic.goto_next)
+
+            vim.api.nvim_create_autocmd('LspAttach', {
+                group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+                callback = function(event)
+                    vim.bo[event.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+                    local opts = { buffer = event.buf }
+
+                    -- Buffer local mappings
+                    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+                    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+                    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+                    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+                    vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
+                    vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
+                    vim.keymap.set('n', '<leader>wl', function()
+                        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+                    end, opts)
+                    vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
+                    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+                    vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
+                    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+                    vim.keymap.set('n', '<leader>f', function()
+                        vim.lsp.buf.format { async = true }
+                    end, opts)
+                end
             })
         end
     }
