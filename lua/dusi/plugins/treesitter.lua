8f3b57b320640c4dcb041681d4884302816fb1cb
Dusernajder <dusungulu.tibi@gmail.com>
2025-02-23 18:04:37 +0100

    changed local tree sitter repo to github repo

--- lua/dusi/plugins/treesitter.lua
+++ lua/dusi/plugins/treesitter.lua
@@ -1,23 +1,21 @@
-return { 
-    "nvim-treesitter/nvim-treesitter", 
+return {
+    "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
         local configs = require("nvim-treesitter.configs")
         configs.setup({
             auto_install = true,
             highlight = { enable = true },
-            indent = { enable = true }
+            indent = { enable = true },
         })
 
-
         -- Treesitter csharp setup
-        local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+        local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
         parser_config.csharp = {
             install_info = {
-                url = "~/.config/tree-sitter/tree-sitter-c-sharp", -- local path or git repo
-                files = {"src/parser.c", "src/scanner.c"}, -- note that some parsers also require src/scanner.c or src/scanner.cc
-            }
+                url = "https://github.com/tree-sitter/tree-sitter-c-sharp", -- local path or git repo
+                files = { "src/parser.c", "src/scanner.c" },    -- note that some parsers also require src/scanner.c or src/scanner.cc
+            },
         }
-
-    end
+    end,
 }
