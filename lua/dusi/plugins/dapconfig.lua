e6dbaf63adb20947a45b0fb7998d6bdaa63b6428
Dusernajder <dusungulu.tibi@gmail.com>
2025-02-27 21:51:29 +0100

    dap/dap-ui and dotnet(netcoredbg) dap support setup

---
+++ lua/dusi/plugins/dapconfig.lua
@@ -0,0 +1,46 @@
+return {
+	"mfussenegger/nvim-dap",
+	dependencies = {
+		"rcarriga/nvim-dap-ui",
+		"nvim-neotest/nvim-nio",
+		"Cliffback/netcoredbg-macOS-arm64.nvim",
+	},
+	config = function()
+		local dap = require("dap")
+		local dapui = require("dapui")
+
+        require("dusi.dap.dotnet")
+
+		dap.listeners.before.attach.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.launch.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.event_terminated.dapui_config = function()
+			dapui.close()
+		end
+		dap.listeners.before.event_exited.dapui_config = function()
+			dapui.close()
+		end
+
+		vim.fn.sign_define("DapBreakpoint", { text = "🟥", texthl = "", linehl = "", numhl = "" })
+		vim.fn.sign_define("DapStopped", { text = "▶️", texthl = "", linehl = "", numhl = "" })
+
+		vim.keymap.set("n", "<Leader>b", function()
+			dap.toggle_breakpoint()
+		end)
+		vim.keymap.set("n", "<F5>", function()
+			dap.continue()
+		end)
+		vim.keymap.set("n", "<F10>", function()
+			dap.step_over()
+		end)
+		vim.keymap.set("n", "<F11>", function()
+			dap.step_into()
+		end)
+		vim.keymap.set("n", "<F12>", function()
+			dap.step_out()
+		end)
+	end,
+}
