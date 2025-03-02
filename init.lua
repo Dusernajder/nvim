20e1c71ce63ece3a2b9f712e5ef0568e65d249f5
Dusernajder <dusungulu.tibi@gmail.com>
2025-03-02 13:32:19 +0100

    restructured setup

--- init.lua
+++ init.lua
@@ -1,26 +1,2 @@
 require("dusi.vimoptions")
-
--- Highlight when yanking (copying) text
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-})
-
--- Lazy setup --
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "--branch=stable",
-        "https://github.com/folke/lazy.nvim.git",
-        lazypath,
-    })
-end
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup("dusi.plugins")
+require("dusi")
