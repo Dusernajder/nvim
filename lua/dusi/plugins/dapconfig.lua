3983b4f5864c46bd27eaabfaf62b508056c6c167
Dusernajder <dusungulu.tibi@gmail.com>
2025-03-02 13:25:49 +0100

    added osx .net support, sign definitions and changed shortcuts

--- lua/dusi/plugins/dapconfig.lua
+++ lua/dusi/plugins/dapconfig.lua
@@ -1,46 +1,70 @@
 return {
-	"mfussenegger/nvim-dap",
-	dependencies = {
-		"rcarriga/nvim-dap-ui",
-		"nvim-neotest/nvim-nio",
-		"Cliffback/netcoredbg-macOS-arm64.nvim",
-	},
-	config = function()
-		local dap = require("dap")
-		local dapui = require("dapui")
+    "mfussenegger/nvim-dap",
+    dependencies = {
+        "rcarriga/nvim-dap-ui",
+        "rcarriga/cmp-dap",
+        "jay-babu/mason-nvim-dap.nvim",
+        "jbyuki/one-small-step-for-vimkind",
+        "nvim-java/nvim-java",
+        "nvim-neotest/nvim-nio",
+        {
+            "Cliffback/netcoredbg-macOS-arm64.nvim",
+            dependencies = { "mfussenegger/nvim-dap" },
+        },
+    },
+    enabled = vim.fn.has("win32") == 0,
+    event = "User BaseFile",
+    config = function()
+        local dap = require("dap")
 
-        require("dusi.dap.dotnet")
+        require("netcoredbg-macOS-arm64").setup(require("dap"))
 
-		dap.listeners.before.attach.dapui_config = function()
-			dapui.open()
-		end
-		dap.listeners.before.launch.dapui_config = function()
-			dapui.open()
-		end
-		dap.listeners.before.event_terminated.dapui_config = function()
-			dapui.close()
-		end
-		dap.listeners.before.event_exited.dapui_config = function()
-			dapui.close()
-		end
+        vim.keymap.set("n", "<Leader>b", function()
+            dap.toggle_breakpoint()
+        end)
+        vim.keymap.set("n", "<Leader>cb", function()
+            local dap = require("dap")
+            vim.ui.input({ prompt = "Enter condition: " }, function(condition)
+                if condition then
+                    dap.set_breakpoint(condition)
+                end
+            end)
+        end)
+        vim.keymap.set("n", "<F5>", function()
+            dap.continue()
+        end)
+        vim.keymap.set("n", "<F12>", function()
+            dap.close()
+        end)
+        vim.keymap.set("n", "<F6>", function()
+            dap.step_over()
+        end)
+        vim.keymap.set("n", "<F7>", function()
+            dap.step_into()
+        end)
+        vim.keymap.set("n", "<F8>", function()
+            dap.step_out()
+        end)
 
-		vim.fn.sign_define("DapBreakpoint", { text = "🟥", texthl = "", linehl = "", numhl = "" })
-		vim.fn.sign_define("DapStopped", { text = "▶️", texthl = "", linehl = "", numhl = "" })
-
-		vim.keymap.set("n", "<Leader>b", function()
-			dap.toggle_breakpoint()
-		end)
-		vim.keymap.set("n", "<F5>", function()
-			dap.continue()
-		end)
-		vim.keymap.set("n", "<F10>", function()
-			dap.step_over()
-		end)
-		vim.keymap.set("n", "<F11>", function()
-			dap.step_into()
-		end)
-		vim.keymap.set("n", "<F12>", function()
-			dap.step_out()
-		end)
-	end,
+        vim.fn.sign_define(
+            "DapBreakpoint",
+            { text = "🛑", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
+        )
+        vim.fn.sign_define(
+            "DapBreakpointCondition",
+            { text = "🔵", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
+        )
+        vim.fn.sign_define(
+            "DapBreakpointRejected",
+            { text = "⚠️", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
+        )
+        vim.fn.sign_define(
+            "DapLogPoint",
+            { text = "📍", texthl = "DapLogPoint", linehl = "DapLogPoint", numhl = "DapLogPoint" }
+        )
+        vim.fn.sign_define(
+            "DapStopped",
+            { text = "▶️", texthl = "DapStopped", linehl = "DapStopped", numhl = "DapStopped" }
+        )
+    end,
 }
