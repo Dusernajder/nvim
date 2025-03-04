7b70178ab5b38ec1494447d907c7cf8ebc123119
Dusernajder <dusungulu.tibi@gmail.com>
2025-03-04 23:24:14 +0100

    neotree mappings

--- lua/dusi/plugins/neotree.lua
+++ lua/dusi/plugins/neotree.lua
@@ -7,12 +7,53 @@ return {
         "MunifTanjim/nui.nvim",
     },
     config = function()
-        vim.keymap.set('n', '<C-n>', function()
-                local command = require("neo-tree.command")
-                command.execute({ toggle = true, source = "filesystem", position = "left" })
-            end,
-            { noremap = true, silent = true }
-        )
+        require("neo-tree").setup({
+            popup_border_style = "rounded",
+            enable_git_status = true,
+            enable_diagnostics = true,
+            window = {
+                position = "left",
+                width = 40,
+                mapping_options = {
+                    noremap = true,
+                    nowait = true,
+                },
+                mappings = {
+                    ["<space>"] = {
+                        "toggle_node",
+                        nowait = true, -- disable `nowait` if you have existing combos starting with this char that you want to use
+                    },
+                    ["<2-LeftMouse>"] = "open",
+                    ["<cr>"] = "open",
+                    ["<esc>"] = "cancel", -- close preview or floating neo-tree window
+                    ["P"] = { "toggle_preview", config = { use_float = true, use_image_nvim = true } },
+                    ["l"] = "focus_preview",
+                    ["S"] = "open_split",
+                    ["s"] = "open_vsplit",
+                    ["t"] = "open_tabnew",
+                    -- ["<cr>"] = "open_drop",
+                    -- ["t"] = "open_tab_drop",
+                    --["P"] = "toggle_preview", -- enter preview mode, which shows the current node without focusing
+                    ["C"] = "close_node",
+                    -- ['C'] = 'close_all_subnodes',
+                    ["z"] = "close_all_nodes",
+                    --["Z"] = "expand_all_nodes",
+                    ["a"] = {
+                        "add",
+                        -- this command supports BASH style brace expansion ("x{a,b,c}" -> xa,xb,xc). see `:h neo-tree-file-actions` for details
+                        -- some commands may take optional config options, see `:h neo-tree-mappings` for details
+                        config = {
+                            show_path = "none", -- "none", "relative", "absolute"
+                        },
+                    },
+                },
+            },
+        })
+
+        vim.keymap.set("n", "<C-n>", function()
+            local command = require("neo-tree.command")
+            command.execute({ toggle = true, source = "filesystem", position = "left" })
+        end, { noremap = true, silent = true })
 
         -- Auto close Neo-tree when opening a file
         vim.api.nvim_create_autocmd("BufEnter", {
@@ -22,7 +63,7 @@ return {
                 if bufname ~= "" and not string.match(bufname, "neo%-tree filesystem") then
                     require("neo-tree.command").execute({ action = "close" })
                 end
-            end
+            end,
         })
-    end
+    end,
 }
