bfab4bbe5ed59ed6c22ac253e6ea96961de55111
Dusernajder <dusungulu.tibi@gmail.com>
2026-01-16 00:17:11 +0100

    ensure omnisharp reload at project launch and clear autocmd

--- init.lua
+++ init.lua
@@ -1,2 +1,5 @@
 require("dusi.vimoptions")
 require("dusi")
+
+package.loaded["lspconfig.server_configurations.omnisharp"] = nil
+vim.api.nvim_clear_autocmds({ group = "lspconfig", event = "FileType", pattern = "cs" })
