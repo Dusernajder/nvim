ca5c37dc5035134fab7942a77cb2b90178f92a18
Dusernajder <dusungulu.tibi@gmail.com>
2026-01-23 13:05:18 +0100

    python completion fix, functionality to import packages from normal mode

--- lua/dusi/plugins/completions.lua
+++ lua/dusi/plugins/completions.lua
@@ -8,82 +8,90 @@ return {
 		"hrsh7th/cmp-nvim-lsp-signature-help",
 		"p00f/clangd_extensions.nvim",
 	},
+
 	{
 		"L3MON4D3/LuaSnip",
-		dependecies = {
+		dependencies = {
 			"saadparwaiz1/cmp_luasnip",
 			"rafamadriz/friendly-snippets",
 		},
 		config = function()
-			-- friendly-snippets - enable standardized comments snippets
-			require("luasnip").filetype_extend("typescript", { "tsdoc" })
-			require("luasnip").filetype_extend("javascript", { "jsdoc" })
-			require("luasnip").filetype_extend("lua", { "luadoc" })
-			require("luasnip").filetype_extend("python", { "pydoc" })
-			require("luasnip").filetype_extend("rust", { "rustdoc" })
-			require("luasnip").filetype_extend("cs", { "csharpdoc" })
-			require("luasnip").filetype_extend("java", { "javadoc" })
-			require("luasnip").filetype_extend("c", { "cdoc" })
-			require("luasnip").filetype_extend("cpp", { "cppdoc" })
-			require("luasnip").filetype_extend("php", { "phpdoc" })
-			require("luasnip").filetype_extend("kotlin", { "kdoc" })
-			require("luasnip").filetype_extend("ruby", { "rdoc" })
-			require("luasnip").filetype_extend("sh", { "shelldoc" })
+			local ls = require("luasnip")
+			require("luasnip.loaders.from_vscode").lazy_load()
+
+			ls.filetype_extend("typescript", { "tsdoc" })
+			ls.filetype_extend("javascript", { "jsdoc" })
+			ls.filetype_extend("lua", { "luadoc" })
+			ls.filetype_extend("python", { "pydoc" })
+			ls.filetype_extend("rust", { "rustdoc" })
+			ls.filetype_extend("cs", { "csharpdoc" })
+			ls.filetype_extend("java", { "javadoc" })
+			ls.filetype_extend("c", { "cdoc" })
+			ls.filetype_extend("cpp", { "cppdoc" })
+			ls.filetype_extend("php", { "phpdoc" })
+			ls.filetype_extend("kotlin", { "kdoc" })
+			ls.filetype_extend("ruby", { "rdoc" })
+			ls.filetype_extend("sh", { "shelldoc" })
 		end,
 	},
+
 	{
 		"hrsh7th/nvim-cmp",
-		opts = {
-			function(_, opts)
-				opts.sources = opts.sources or {}
-				table.insert(opts.sources, {
-					name = "lazydev",
-					group_index = 0,
-				})
-			end,
-		},
 		config = function()
 			local cmp = require("cmp")
-			require("luasnip.loaders.from_vscode").lazy_load()
+
 			require("lazydev").setup({
 				library = { "nvim-dap-ui" },
 			})
 
 			cmp.setup({
 				snippet = {
-					-- REQUIRED - you must specify a snippet engine
 					expand = function(args)
 						require("luasnip").lsp_expand(args.body)
 					end,
 				},
+
 				experimental = {
-					ghost_text = {
-						-- You can optionally specify a highlight group.
-						hl_group = "Comment",
-					},
+					ghost_text = { hl_group = "Comment" },
 				},
+
 				window = {
 					completion = cmp.config.window.bordered(),
 					documentation = cmp.config.window.bordered(),
 				},
+
 				mapping = cmp.mapping.preset.insert({
 					["<C-b>"] = cmp.mapping.scroll_docs(-4),
 					["<C-f>"] = cmp.mapping.scroll_docs(4),
 					["<C-x>"] = cmp.mapping.complete(),
 					["<C-e>"] = cmp.mapping.abort(),
-					-- ["<Tab>"] = cmp.mapping.complete({ select = true }),
-					["<CR>"] = cmp.mapping.confirm({
-						behavior = cmp.ConfirmBehavior.Replace,
-						select = true,
-					}),
+
+					-- critical: do not auto-select
+					["<CR>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							local entry = cmp.get_selected_entry()
+							if entry then
+								cmp.confirm({
+									behavior = cmp.ConfirmBehavior.Replace,
+									select = false,
+								})
+							else
+								fallback()
+							end
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
 				}),
+
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
-					{ name = "nvim_lsp_signature_help" },
-					{ name = "luasnip" }, -- For luasnip users.
+					-- { name = "nvim_lsp_signature_help" },
+					{ name = "luasnip" },
 				}, {
 					{ name = "buffer" },
 				}),
+
 				sorting = {
 					comparators = {
 						cmp.config.compare.offset,
@@ -96,18 +104,67 @@ return {
 						cmp.config.compare.order,
 					},
 				},
+
 				formatting = {
 					format = function(entry, vim_item)
-						-- Add import text to completion menu
 						if entry.source.name == "nvim_lsp" then
-							local completion_item = entry.completion_item
-							if completion_item.additionalTextEdits then
+							if entry.completion_item.additionalTextEdits then
 								vim_item.menu = "[+Import]"
 							end
 						end
 						return vim_item
 					end,
 				},
+			})
+
+			-- Python-only: trigger import suggestions for symbol under cursor
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = "python",
+				callback = function()
+					vim.keymap.set("n", "<leader>ii", function()
+						local cmp = require("cmp")
+						local word = vim.fn.expand("<cword>")
+						if word == "" then
+							return
+						end
+
+						local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+						local line = vim.api.nvim_get_current_line()
+						col = col + 1 -- 1-based
+
+						-- find word boundaries safely
+						local start_col = col
+						while start_col > 1 and line:sub(start_col - 1, start_col - 1):match("[%w_]") do
+							start_col = start_col - 1
+						end
+
+						local end_col = col
+						while end_col <= #line and line:sub(end_col, end_col):match("[%w_]") do
+							end_col = end_col + 1
+						end
+
+						start_col = start_col - 1
+						end_col = end_col - 1
+						if start_col >= end_col then
+							return
+						end
+
+						-- rewrite symbol (no visible change, but triggers completion context)
+						vim.api.nvim_buf_set_text(0, row - 1, start_col, row - 1, end_col, { word })
+
+						-- ENTER INSERT MODE, then trigger cmp
+						vim.api.nvim_feedkeys("a", "n", false)
+						vim.schedule(function()
+							cmp.complete({
+								config = {
+									sources = {
+										{ name = "nvim_lsp" },
+									},
+								},
+							})
+						end)
+					end, { buffer = true, desc = "Import symbol under cursor" })
+				end,
 			})
 		end,
 	},
