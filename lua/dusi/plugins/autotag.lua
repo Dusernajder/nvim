a696c060708da8482b9716b1d006bcf4e3f99337
Dusernajder <dusungulu.tibi@gmail.com>
2026-01-26 11:49:45 +0100

    autotag support added for multiple files, types of tags

--- lua/dusi/plugins/autotag.lua
+++ lua/dusi/plugins/autotag.lua
@@ -1,12 +1,20 @@
 return {
 	"windwp/nvim-ts-autotag",
-	dependencies = { "nvim-treesitter/nvim-treesitter" },
+	event = "InsertEnter",
 	config = function()
 		require("nvim-ts-autotag").setup({
-			opts = {
-				enable_close = true, -- auto close tags
-				enable_rename = true, -- auto rename pairs
-				enable_close_on_slash = false,
+			enable = true,
+			enable_rename = true,
+			enable_close = true,
+			enable_close_on_slash = false,
+			filetypes = {
+				"html",
+				"xml",
+				"javascript",
+				"typescript",
+				"typescriptreact",
+				"javascriptreact",
+				"angular",
 			},
 		})
 	end,
