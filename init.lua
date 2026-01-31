d0e817b8ef11c919b6ae7883515e97a2cd751970
Dusernajder <dusungulu.tibi@gmail.com>
2026-01-31 16:02:13 +0100

    custom c# buffer clear/attachment removed, no need after coc

--- init.lua
+++ init.lua
@@ -1,8 +1,2 @@
 require("dusi.vimoptions")
 require("dusi")
-
-package.loaded["lspconfig.server_configurations.omnisharp"] = nil
-vim.api.nvim_clear_autocmds({
-    event = "FileType",
-    pattern = "cs",
-})
