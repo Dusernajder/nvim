ca1abe57ec3fed5ca2c12ccd3696ffa5f0122e56
Dusernajder <dusungulu.tibi@gmail.com>
2026-01-31 21:03:33 +0100

    coc extensions

---
+++ lua/dusi/plugins/coc/extensions.lua
@@ -0,0 +1,28 @@
+vim.g.coc_global_extensions = {
+    -- core
+    "coc-json",
+    "coc-emmet",
+
+    -- linters
+    "coc-html",
+    "coc-htmlhint",
+    "coc-css",
+
+    -- misc / completions
+    "coc-class-css",
+    "coc-tailwindcss",
+
+    -- frameworks
+    "coc-angular",
+
+    -- language servers
+    "coc-omnisharp",
+    "coc-pyright",
+    "coc-lua",
+    "coc-tsserver",
+    "coc-clangd",
+    "coc-java",
+
+    -- formatters
+    "coc-prettier",
+}
