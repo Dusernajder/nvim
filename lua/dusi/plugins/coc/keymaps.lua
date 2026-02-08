7a0fa43afc19d3346bae6902fea37f60fa9896b4
Dusernajder <dusungulu.tibi@gmail.com>
2026-02-08 12:54:08 +0100

    code completion trigger new keymap <C-s>, removed unused maps

--- lua/dusi/plugins/coc/keymaps.lua
+++ lua/dusi/plugins/coc/keymaps.lua
@@ -13,19 +13,26 @@ vim.keymap.set("i", "<C-k>", "<Cmd>call CocActionAsync('showSignatureHelp')<CR>"
 -- vim.keymap.set("n", "K", '<CMD>lua _G.show_docs()<CR>', { silent = true })
 vim.keymap.set("n", "<leader>D", "<Plug>(coc-type-definition)", { desc = "Type definition" })
 vim.keymap.set("n", "<leader>rn", "<Plug>(coc-rename)", { desc = "Rename symbol" })
-vim.keymap.set({ "n", "v" }, "<leader>ca", "<Plug>(coc-codeaction-selected)", { desc = "Code action" })
 vim.keymap.set("n", "gr", "<Plug>(coc-references)", { desc = "References" })
 vim.keymap.set("n", "<leader>f", "<Cmd>call CocActionAsync('format')<CR>", { desc = "Format" })
+
+-- Code actions
+vim.keymap.set("n", "<leader>ca", "<Plug>(coc-codeaction-line)", { desc = "Code action (line)" })
+vim.keymap.set("v", "<leader>ca", "<Plug>(coc-codeaction-selected)", { desc = "Code action (selection)" })
 vim.keymap.set("n", "<leader>m", "<Plug>(coc-codeaction-source)", { desc = "Source code action (organize imports)" })
+vim.keymap.set("n", "<leader>qi", function()
+    vim.fn.CocActionAsync("codeAction", "quickfix")
+end, { desc = "Quick fix" })
 
 
 -- Completion keymaps
 vim.cmd([[
     inoremap <silent><expr> <CR> coc#pum#visible() ? coc#pum#confirm() : "\<CR>"
-    inoremap <silent><expr> <C-Space> coc#refresh()
-    inoremap <silent><expr> <C-n> coc#pum#visible() ? coc#pum#next(1) : "\<C-n>"
-    inoremap <silent><expr> <C-p> coc#pum#visible() ? coc#pum#prev(1) : "\<C-p>"
+    "inoremap <silent><expr> <C-n> coc#pum#visible() ? coc#pum#next(1) : "\<C-n>"
+    "inoremap <silent><expr> <C-p> coc#pum#visible() ? coc#pum#prev(1) : "\<C-p>"
+
     inoremap <silent><expr> <Tab> coc#pum#visible() ? coc#pum#next(1) : "\<Tab>"
     inoremap <silent><expr> <S-Tab> coc#pum#visible() ? coc#pum#prev(1) : "\<S-Tab>"
-    inoremap <silent><expr> <leader><leader> coc#refresh()
+
+    inoremap <silent><expr> <C-s> coc#refresh()
 ]])
