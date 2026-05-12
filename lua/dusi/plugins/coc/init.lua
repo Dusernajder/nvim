4ad565457e4835d6c51b4689aea6b7d23674c1ea
Dusernajder <dusungulu.tibi@gmail.com>
2026-05-12 23:27:17 +0200

    fix: csharp-ls restarts on CocDiagnosticChange (this change needed to ensure csharp-ls start correctly)

--- lua/dusi/plugins/coc/init.lua
+++ lua/dusi/plugins/coc/init.lua
@@ -11,5 +11,19 @@ return {
 
         -- Load keymaps
         require("dusi.plugins.coc.keymaps")
+
+        -- Auto-restart C# language server when opening a .NET project
+        local csharp_restarted = false
+        vim.api.nvim_create_autocmd({ "User" }, {
+            pattern = "CocDiagnosticChange",
+            callback = function()
+                if not csharp_restarted and vim.bo.filetype == "cs" then
+                    vim.defer_fn(function()
+                        vim.cmd("CocCommand dotnet.restartServer")
+                        csharp_restarted = true
+                    end, 500)
+                end
+            end,
+        })
     end,
 }
