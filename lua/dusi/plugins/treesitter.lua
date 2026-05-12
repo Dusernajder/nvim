26aaec47c34b5e53e242c6ffd9424a8cb94d52b9
Dusernajder <dusungulu.tibi@gmail.com>
2026-05-12 15:42:32 +0200

    feature: c_sharp added to treesitter parser

--- lua/dusi/plugins/treesitter.lua
+++ lua/dusi/plugins/treesitter.lua
@@ -10,6 +10,7 @@ return {
             ensure_installed = {
                 "bash",
                 "c",
+                "c_sharp",
                 "diff",
                 "html",
                 "javascript",
